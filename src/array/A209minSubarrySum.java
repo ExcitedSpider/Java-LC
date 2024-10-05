86a1ea69e54719d97a37c7ff5f1e15e368713dae
Chew F <chew.y.feng@outlook.com>
2024-10-05 17:47:37 +1000

    perf: a209 improved version

--- src/array/A209minSubarrySum.java
+++ src/array/A209minSubarrySum.java
@@ -33,13 +33,41 @@ public class A209minSubarrySum {
     }else {
       return getSum.apply(0, nums.length) >= target ? minLength : 0;
     }
+  }
 
-    
+  // speed up version
+  // need to keep track of sum 
+  public static int minSubArrayLen0(int target, int[] nums) {
+    var minLength = nums.length;
+    var sum = 0;
+    int left = 0, right = 0;
+
+    while (right < nums.length) {
+      if(sum < target) {
+        sum += nums[right];
+        right ++;
+      } else {
+        minLength = Math.min(right - left, minLength); 
+        sum -= nums[left];
+        left ++;
+      }
+    }
+
+    while (sum >= target) {
+        minLength = Math.min(right - left, minLength); 
+        sum -= nums[left];
+        left ++;
+    }
+
+    if(left == 0 && right == nums.length && sum < target) {
+      return 0;
+    }
+    return minLength;
   }
 
   public static void main(String[] args) {
     int[] testCase1 = {2,3,1,2,4,3};
-    System.err.println(minSubArrayLen(7, testCase1));
+    System.err.println(minSubArrayLen0(7, testCase1));
 
     // int[] testCase2 = {1,4,4};
     // System.err.printf("You get %d \n", minSubArrayLen(4, testCase2));
