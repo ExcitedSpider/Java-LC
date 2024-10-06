b49a407177b66d551d56eafe5bbbec1731848add
Chew F <chew.y.feng@outlook.com>
2024-10-06 17:21:08 +1100

    feat: a160

---
+++ src/linkedlist/A160Intersection.java
@@ -0,0 +1,52 @@
+package linkedlist;
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+public class A160Intersection {
+  static int length(ListNode head) {
+    var l = 0;
+    while (head!=null) {
+      head =head.next;
+      l ++;
+    }
+    return l;
+  }
+
+  public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    // The property of A and B:
+    // the ends in the same nodes
+    var lenA = length(headA);
+    var lenB = length(headB);
+
+    ListNode longerList = lenA > lenB? headA : headB;
+    ListNode shorterList = longerList == headA ? headB: headA;
+    var diff = Math.abs(lenA - lenB);
+    while(diff != 0) {
+      longerList = longerList.next;
+      diff --;
+    }
+
+    var ptrA = longerList;
+    var ptrB = shorterList;
+
+    while (ptrA != ptrB) {
+      ptrA = ptrA.next;
+      ptrB = ptrB.next;
+    }
+    return ptrA;
+  }
+}
