aeb24490d5afc2b052a3a961311606e876d5f8a3
Chew F <chew.y.feng@outlook.com>
2024-10-11 16:07:05 +1100

    feat: a343

---
+++ src/dp/A343IntegerBreak.java
@@ -0,0 +1,24 @@
+package dp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class A343IntegerBreak {
+  public int integerBreak(int n) {
+    return integerBreak(n, new HashMap<>());
+  }
+
+  public int integerBreak(int n, Map<Integer, Integer> memo) {
+      if(n <= 2) return 1;
+      if(memo.containsKey(n)) return memo.get(n);
+      
+      var res = 0;
+      for(var i = 1; i < n; i ++) {
+        res = Math.max(res, 
+            Math.max(i * (n - i), i * integerBreak(n - i, memo)));
+      }
+      memo.put(n, res);
+
+      return res;
+  }
+}
