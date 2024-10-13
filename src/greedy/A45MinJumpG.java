aca9361fa6eeeecec809579b249d96e398d442cb
Chew F <chew.y.feng@outlook.com>
2024-10-13 16:32:02 +1100

    fix: more elegant

--- src/greedy/A45MinJumpG.java
+++ src/greedy/A45MinJumpG.java
@@ -5,28 +5,19 @@ public class A45MinJumpG {
   public int jump(int[] nums) {
     if(nums.length == 1) return 0;
 
-    var preRange = 0;
-    var curRange = 0;
-    var minStep = 0;
-
-    while(curRange < nums.length - 1) {
-      var it = nextCoverRange(nums, preRange, curRange);
-      preRange = curRange;
-      curRange = it;
-      minStep ++;
-    }
-
-    return minStep;
+    return jump(nums, 0, 0, 0);
   }
 
   // get next cover in [L, R] range 
-  private int nextCoverRange(int[] nums, int curRangeL, int curRangeR) {
+  private int jump(int[] nums, int curRangeL, int curRangeR, int step) {
+    if(curRangeR >= nums.length - 1) return step;
+
     var nextRange = curRangeR;
     for(var i = curRangeL; i <= curRangeR; i ++) {
       nextRange = Math.max(nextRange, nums[i] + i);
     }
 
-    return nextRange;
+    return jump(nums, curRangeR + 1, nextRange, step + 1);
   }
 
   public static void main(String[] args) {
