8b785da19122cb55445287da744032c3cc619f68
Chew F <chew.y.feng@outlook.com>
2024-10-13 15:05:20 +1100

    perf: a55 greedy

--- src/greedy/A55JumpGame.java
+++ src/greedy/A55JumpGame.java
@@ -2,15 +2,17 @@ package greedy;
 
 public class A55JumpGame {
   public boolean canJump(int[] nums) {
-    if (nums.length == 1) return true;
-    var maxCover = 0;
+    if(nums.length <= 1) return true;
 
-    for (int i = 0; i <= maxCover; i++) {
-      maxCover = Math.max(maxCover, nums[i] + i);
-      if(maxCover >= nums.length - 1) {
-        return true;
-      }
-    }
-    return false;
+    var maxRange = getMaxCover(nums, 0, nums[0]);
+    return maxRange >= nums.length - 1;
+  }
+
+  private int getMaxCover(int[] nums, int pos, int rangeCover) {
+    if(rangeCover >= nums.length -1 || pos > rangeCover) return rangeCover;
+
+    var nextRange = pos + nums[pos];
+    if(nextRange > rangeCover) return getMaxCover(nums, pos + 1, nextRange);
+    else return getMaxCover(nums, pos + 1, rangeCover);
   }
 }
