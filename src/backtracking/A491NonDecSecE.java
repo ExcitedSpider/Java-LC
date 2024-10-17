edec8a0a9bcd45e77356472c135826ab93c46f82
Chew F <chew.y.feng@outlook.com>
2024-10-17 15:20:34 +1100

    fix: a491 bugs and provide an efficient version

--- src/backtracking/A491NonDecSecE.java
+++ src/backtracking/A491NonDecSecE.java
@@ -3,8 +3,11 @@ package backtracking;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public class A491NonDecSec {
+// efficient remove duplicates
+// not involve sets
+public class A491NonDecSecE {
   List<List<Integer>> ans;
 
   public List<List<Integer>> findSubsequences(int[] nums) {
@@ -12,15 +15,18 @@ public class A491NonDecSec {
     findSubsequences0(nums, 0, -1, new ArrayList<>());
 
     // this trick to remove duplicates works, but not so efficient
-    return new ArrayList<>(new HashSet<>(ans));
+    return ans;
   }
 
   private void findSubsequences0(int[] nums, int start, int last, List<Integer> path) {
     if(start > nums.length) return;
     if(path.size() > 1) ans.addLast(new ArrayList<>(path));
 
+    Set<Integer> used = new HashSet<>();
+
     for (int i = start; i < nums.length; i++) {
-      if(last < 0 || nums[i] >= nums[last]) {
+      if(!used.contains(nums[i]) && (last < 0 || nums[i] >= nums[last])) {
+        used.add(nums[i]);
         path.addLast(nums[i]);
 
         findSubsequences0(nums, i + 1, i, path);
