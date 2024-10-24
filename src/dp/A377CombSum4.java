f84e9639a11986bfa1f816a8fc18d64a19055bfe
Chew F <chew.y.feng@outlook.com>
2024-10-24 14:48:10 +1100

    feat: a377

--- src/dp/A377CombSum4.java
+++ src/dp/A377CombSum4.java
@@ -7,23 +7,24 @@ import java.util.Arrays;
  */
 public class A377CombSum4 {
   public int combinationSum4(int[] nums, int target) {
-    int[] memo = new int[target];
+    int[] memo = new int[target + 1];
     Arrays.fill(memo, -1);
 
     return combSum(nums, target, memo);
       
   }
 
-  // how many possible ways to formulate target
+  // combSum(nums, target): how many possible ways to get the sum target 
+  // by combination of nums
   private int combSum(int[] nums, int target, int [] memo) {
     if(target < 0) return 0;
     if(target == 0) return 1; // only the empty set
     if(memo[target] != -1) return memo[target];
-    
-    var res = 0;
-    for (int i = 0; i < nums.length; i++) {
-      res += combSum(nums, target - nums[i], memo);
-    }
+
+    var res = Arrays
+              .stream(nums)
+              .map(val -> combSum(nums, target - val, memo))
+              .sum();
 
     memo[target] = res;
     return res;
