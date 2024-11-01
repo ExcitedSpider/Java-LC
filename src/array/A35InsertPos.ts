a409f067c88d069f523e50f69513eaf1759ac9d4
Chew F <chew.y.feng@outlook.com>
2024-11-01 12:45:08 +1100

    fix: doc

--- src/array/A35InsertPos.ts
+++ src/array/A35InsertPos.ts
@@ -3,7 +3,7 @@ function searchInsert(nums: number[], target: number): number {
   return bsearch(0, nums.length - 1);
 
   function bsearch(left: number, right: number): number {
-    if (left > right) return left; // only happened when left == 0 && right == 1
+    if (left > right) return left; // only happened when left - right == 1 due to integer arith
     if (left === right) {
       if (nums[left] < target) return left + 1;
       else return left;
