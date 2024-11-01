8adb7332f58d0627dbce3f4b481d451a6dfd338a
Chew F <chew.y.feng@outlook.com>
2024-11-01 12:23:00 +1100

    feat: a35

---
+++ src/array/A35InsertPos.ts
@@ -0,0 +1,17 @@
+function searchInsert(nums: number[], target: number): number {
+
+  return bsearch(0, nums.length - 1);
+
+  function bsearch(left: number, right: number): number {
+    if (left > right) return left; // only happened when left == 0 && right == 1
+    if (left === right) {
+      if (nums[left] < target) return left + 1;
+      else return left;
+    };
+
+    const mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] === target) return mid;
+    if (nums[mid] > target) return bsearch(left, mid - 1);
+    else return bsearch(mid + 1, right);
+  }
+};
