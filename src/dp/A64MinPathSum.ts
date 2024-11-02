b7be93013bb267b55111cf0f3b4167f5a2b4df35
Chew F <chew.y.feng@outlook.com>
2024-11-02 17:24:49 +1100

    feat: a64

--- src/dp/A64MinPathSum.ts
+++ src/dp/A64MinPathSum.ts
@@ -4,19 +4,18 @@ function minPathSum(grid: number[][]): number {
   const n = grid[0].length;
   const memo: number[][] = Array.from({ length: m }, () => new Array(n).fill(-1));
 
-  return search(m - 1, n - 1) + grid[m-1][n-1];
+  return search(m - 1, n - 1);
 
+  // the cost to (m, n)
   function search(m: number, n: number): number {
-    if (m === 0 && n === 0) return 0;
+    if (m === 0 && n === 0) return grid[0][0];
     if (memo[m][n] !== -1) return memo[m][n];
-
-    const res = Math.min(
-      m === 0 ? Number.MAX_SAFE_INTEGER : search(m - 1, n) + grid[m - 1][n],
-      n === 0 ? Number.MAX_SAFE_INTEGER : search(m, n - 1) + grid[m][n - 1],
+    
+    const res = grid[m][n] + Math.min(
+      m === 0 ? Number.MAX_SAFE_INTEGER : search(m - 1, n),
+      n === 0 ? Number.MAX_SAFE_INTEGER : search(m, n - 1),
     )
     memo[m][n] = res;
     return res;
   }
 };
-
-minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]])
